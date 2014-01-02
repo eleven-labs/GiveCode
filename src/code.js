9bc0150172de2097694efc9b10f5623657415ce3
OpenShift guest <jalouz.j@gmail.com>
2014-01-02 17:10:13 -0500

    Add binding end setter getter

--- src/code.js
+++ src/code.js
@@ -9,8 +9,46 @@
                 keywords : "php, hello"
             },
 
-            initialize : function Post() {
-                console.log('Post Constructor');
+            initialize : function Doc() {
+                console.log('Doc Constructor');
+            
+                this.bind("invalid", function(model, error){
+                    console.log( error );
+                });
+            },
+
+            validate: function( attributes ){
+                if( attributes.title === '') {
+                    return "Le titre du document ne peut pas être vide !!!";
+                }
+            },
+            
+            getId : function() {
+                return this.get('id');
+            },
+            setId : function(value) {
+                this.set({ id : value });
+            },
+    
+            getTitle : function() {
+                return this.get('title');
+            },
+            setTitle : function(value) {
+                this.set({ title : value }, {validate: true});
+            },
+    
+            getCode : function() {
+                return this.get('code');
+            },
+            setCode : function(value) {
+                this.set({ code : value });
+            },
+    
+            getKeywords : function() {
+                return this.get('keywords');
+            },
+            setKeywords : function(value) {
+                this.set({ keywords : value });
             }
         });
 
